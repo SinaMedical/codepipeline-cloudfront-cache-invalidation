b6e76890932fa35b48ac39a26ea331eb86d80148
Pranay Kotecha <pranay@pranaykotecha.com>
2021-05-19 17:13:45 +0100

    Fetch distribution from user params within event

--- cfn-cache-codepipeline.py
+++ cfn-cache-codepipeline.py
@@ -5,13 +5,10 @@ import json
 
 pipeline = boto3.client('codepipeline')
 
-print("getting environment variable")
-print("environment variable: " + os.environ['DISTRIBUTION_ID'])
-dist = os.environ['DISTRIBUTION_ID'] #// get the Distribution ID from the lambda environment variable
-# dist_str = str(dist) //statement to convert the distribution variable into a string
-
-
 def lambda_handler(event, context):
+    dist = event["CodePipeline.job"]["data"]["actionConfiguration"]["configuration"]["UserParameters"]
+    print("Invalidating distribution: " + dist)
+
     client = boto3.client('cloudfront')
     invalidation = client.create_invalidation(
         DistributionId=dist,
@@ -24,12 +21,12 @@ def lambda_handler(event, context):
         },
         'CallerReference': str(time.time())
     })
-    
+
     response = pipeline.put_job_success_result(
         jobId=event['CodePipeline.job']['id']
     )
+
     return {
-    'statusCode': 200,
-    'body': json.dumps('Successully created cache invalidation for ') + dist,
-        
+        'statusCode': 200,
+        'body': json.dumps('Successully created cache invalidation for ') + dist
     }
